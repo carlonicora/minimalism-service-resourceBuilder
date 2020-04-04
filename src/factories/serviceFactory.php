a2398d3dad82f299c1313430ddca9232261b25cb
Carlo Nicora <carlo.nicora@gmail.com>
2020-04-04 14:44:32 +0100

    initial commit

---
+++ src/factories/serviceFactory.php
@@ -0,0 +1,27 @@
+<?php
+namespace carlonicora\minimalism\services\resourceBuilder\factories;
+
+use carlonicora\minimalism\core\services\abstracts\abstractServiceFactory;
+use carlonicora\minimalism\core\services\factories\servicesFactory;
+use carlonicora\minimalism\services\resourceBuilder\configurations\resourceBuilderConfigurations;
+use carlonicora\minimalism\services\resourceBuilder\resourceBuilder;
+
+class serviceFactory extends abstractServiceFactory {
+    /**
+     * serviceFactory constructor.
+     * @param servicesFactory $services
+     */
+    public function __construct(servicesFactory $services) {
+        $this->configData = new resourceBuilderConfigurations();
+
+        parent::__construct($services);
+    }
+
+    /**
+     * @param servicesFactory $services
+     * @return resourceBuilder
+     */
+    public function create(servicesFactory $services) : resourceBuilder {
+        return new resourceBuilder($this->configData, $services);
+    }
+}
